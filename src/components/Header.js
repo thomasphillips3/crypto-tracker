3329aee2e646d1588bd34ad598ebf1c6c2c5be79
Thomas Phillips <thomasphillips3@gmail.com>
2018-12-07 15:05:44 -0800

    Initial commit.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Header extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Crypto Tracker</Text>
+      </View>
+
+    )
+  }
+}
+
+export default Header;
